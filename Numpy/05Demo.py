7e486c9dd34ee6107310609cc626c83d1dad3666
zhoubenjian <bjzhou@outlook.com>
2026-01-06 10:30:33 +0800

    Complete SVD in 07Demo.py Numpy

--- Numpy/05Demo.py
+++ Numpy/05Demo.py
@@ -6,16 +6,16 @@ import numpy as np
 
 a = np.array([-1, -3, 2, -7, 0, 5])
 # 负数 -> -1; 正数 -> 1；零 -> 0
-print(np.sign(a))  # [-1 -1  1 -1  0  1]
+print(np.sign(a))   # [-1 -1  1 -1  0  1]
 
 print('\n' + '=' * 30)
 
 X = np.array([2, 3, 5, 7, 11, 13, 17, 19])
-print(X.shape)  # (8,)
+print(X.shape)      # (8,)
 y = np.arange(1, 9).reshape(8, -1)
-print(y.shape)  # (8, 1)
+print(y.shape)      # (8, 1)
 
-print('##############################')
+print('\n' + '#' * 30)
 
 for i, (xi, yi) in enumerate(zip(X, y)):
     print(f'{i + 1}: {xi:2d} -> {yi}')
---
+++ Numpy/07Demo.py
@@ -0,0 +1,29 @@
+'''
+奇异值分解
+'''
+import numpy as np
+
+
+A = np.array([
+    [3, 2, 2],
+    [2, 3, -2]
+], dtype=float)
+
+# 奇异值分解
+U, s, VT = np.linalg.svd(A)
+print('s =', s)
+# 创建与 A 同型的零矩阵
+Sigma = np.zeros_like(A, dtype=float)
+# 将 s 填入主对角线
+np.fill_diagonal(Sigma, s)
+
+print('U = \n', U)
+print('Singular values = ', s)
+print('V^T = \n', VT)
+
+print("\n重构的矩阵 A_reconstructed:")
+A_reconstructed = U @ Sigma @ VT
+print(A_reconstructed)
+
+# 校验误差
+print(f'重构误差：{np.linalg.norm(A - A_reconstructed):.10f}')
