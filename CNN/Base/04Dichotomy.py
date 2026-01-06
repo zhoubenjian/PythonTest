aa5d792e43f5a2731024f4ffb09a6cbb9b5ea543
zhoubenjian <bjzhou@outlook.com>
2026-01-06 17:25:36 +0800

    add 04Dichotomy in CNN

---
+++ CNN/Base/04Dichotomy.py
@@ -0,0 +1,43 @@
+'''
+感知机（二分法示例）
+'''
+import numpy as np
+
+
+class Perceptron:
+    def __init__(self, lr = 0.01, epochs = 1000):
+        self.lr = lr
+        self.epochs = epochs
+        self.w = None
+        self.b = 0
+
+    # 激活函数
+    def activation(self, z):
+        return np.where(z > 0, 1, -1)
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.w = np.zeros(n_features)
+        for _ in range(self.epochs):
+            for i, x_i in enumerate(X):
+                z = np.dot(x_i, self.w) + self.b
+                y_pred = self.activation(z)
+
+                # 分类错误
+                if y[i] * y_pred <= 0:
+                    self.w += self.lr * y[i] * x_i
+                    self.b += self.lr * y[i]
+    def predict(self, X):
+        z = np.dot(X, self.w) + self.b
+        return self.activation(z)
+
+
+
+# 测试：线性可分数据（AND逻辑）
+X = np.array([[0,0], [0,1], [1,0], [1,1]])
+# AND标签
+y = np.array([-1, -1, -1, 1])
+model = Perceptron(lr=0.1, epochs=100)
+model.fit(X, y)
+print("权重:", model.w, "偏置:", model.b)
+print("预测:", model.predict(X))
