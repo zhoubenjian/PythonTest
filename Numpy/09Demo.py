72cca3e1dcf44ccb22377e6c1027f3ec25bf07d2
zhoubenjian <bjzhou@outlook.com>
2026-03-23 17:11:13 +0800

    Review something...

--- Numpy/09Demo.py
+++ Numpy/09Demo.py
@@ -15,7 +15,8 @@ print(array01)
 行向量
 '''
 array02 = array01.reshape(1, -1)
-print('行向量：%s' % array02)       # 行向量：[[0 1 2 3]]
+print('行向量：%s' % array02)           # 行向量：[[0 1 2 3]]
+print(f'行向量形状：{array02.shape}')     # 行向量形状：(1, 4)
 
 '''
 列向量
@@ -29,6 +30,7 @@ array03 = array01.reshape(-1, 1)
  [3]]
 '''
 print('列向量：\n%s' % array03)
+print(f'列向量形状：{array03.shape}')     # 列向量形状：(4, 1)
 
 
 print('----------------------------')
