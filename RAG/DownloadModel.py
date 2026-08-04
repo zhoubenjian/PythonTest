0326c2f63a91182c96165bc9d1d0967092df021e
zhoubenjian <bjzhou@outlook.com>
2026-08-04 16:04:11 +0800

    change local_model_dir: the absolute path to a relative path...

--- RAG/DownloadModel.py
+++ RAG/DownloadModel.py
@@ -5,12 +5,14 @@ import os
 
 # 必须全部放在导入huggingface_hub之前！顺序不能乱
 os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
-os.environ["HF_HUB_DISABLE_XET"] = "1"   # 关键：禁用XET，解决401报错
+# 关键：禁用XET，解决401报错
+os.environ["HF_HUB_DISABLE_XET"] = "1"
 
 from huggingface_hub import snapshot_download
 
+
 repo_id = "sentence-transformers/all-MiniLM-L6-v2"
-local_model_dir = r"D:\PythonProject\Github\PythonTest\local_models\all-MiniLM-L6-v2"
+local_model_dir = r"../local_models/all-MiniLM-L6-v2"
 
 snapshot_download(
     repo_id=repo_id,
