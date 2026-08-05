f8d306cb40336753466a05eb92697e7844cc4501
zhoubenjian <bjzhou@outlook.com>
2026-08-05 10:15:20 +0800

    Download embedding model to loacl: bge-small-zh-v1.5

--- RAG/DownloadModel.py
+++ RAG/DownloadModel.py
@@ -1,5 +1,13 @@
 '''
-从HuggingFace上手动下载模型，保存到本地指定目录。
+从HuggingFace上手动下载指定模型 保存到本地指定目录
+
+
+向量化模型：
+    all-MiniLM-L6-v2
+        轻量英文向量化模型，速度快，注意：对中文效果一般
+
+    bge-small-zh-v1.5
+        对中文友好
 '''
 import os
 
@@ -11,10 +19,10 @@ os.environ["HF_HUB_DISABLE_XET"] = "1"
 from huggingface_hub import snapshot_download
 
 
-repo_id = "sentence-transformers/all-MiniLM-L6-v2"
-local_model_dir = r"../local_models/all-MiniLM-L6-v2"
+repo_id = "BAAI/bge-small-zh-v1.5"    # "sentence-transformers/all-MiniLM-L6-v2"
+local_model_dir = r"../local_models/bge-small-zh-v1.5"    # r"../local_models/all-MiniLM-L6-v2"
 
 snapshot_download(
-    repo_id=repo_id,
-    local_dir=local_model_dir,
+    repo_id = repo_id,
+    local_dir = local_model_dir,
 )
